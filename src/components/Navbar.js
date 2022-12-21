455308ea6f315822847a76f58fac4309c412680e
Kevin Aton <kevinaton@Kevins-MacBook-Pro.local>
2022-12-22 01:07:02 +0800

    added components. resolve compatibility issue in node and script

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div>
+            <Typography>Navbar</Typography>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/theme.js
@@ -0,0 +1,31 @@
+import { createTheme } from "@mui/material/styles";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+export const theme = createTheme({
+    palette:{
+        primary:{
+            main:'#82AAE3',
+            light:'#BFEAF5'
+        },
+        secondary:{
+            main:'#91D8E4',
+            light:'#EAFDFC'
+        }
+    },
+    typography: {
+        fontFamily: [
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            '"Segoe UI"',
+            'BlinkMacSystemFont',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    },
+})
