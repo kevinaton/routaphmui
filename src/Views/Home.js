36674da9dd12c7d438bf47e229a1e110697b031e
Kevin Aton <kevinaton@Kevins-MacBook-Pro.local>
2023-02-08 00:58:31 +0800

    added current location and resolved getting autocomplete input

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
+import React, { useEffect } from 'react'
+import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
 import { Box } from '@mui/system';
 import { Autocomplete, Grid, IconButton, InputAdornment, TextField, Typography } from '@mui/material';
 import data from "../assets/masterList.json"
 import { Search } from '@mui/icons-material';
+import { useNavigate } from 'react-router-dom';
 
 const {MasterList} = data
 
@@ -23,59 +24,42 @@ const containerStyle = {
   height: '100%'
 };
 
-const zoom = 6
-
 const center = {
   lat: 12.037028832474519,
   lng: 122.91838000532566
 };
 
-const markers = [
-  {name:'Cagayan de Oro City', position:{lat: 8.468116915462604, lng: 124.64139117461927}},
-  {name:'Cebu City', position:{lat: 10.39344668412612, lng: 123.77195816886562}},
-];
+const markers = MasterList[0].submenu
 
-// const markerOnLoad = marker => {
-//   console.log('marker: ', marker)
-// };
-
-const SearchBar = () => (
-  <Autocomplete
-    freeSolo={true}
-    id="free-solo-2-demo"
-    disableClearable
-    options={searchLists}
-    renderInput={(params) => (        
-        <TextField
-          {...params}
-          label="Search a city, location, route name,"
-          InputProps={{
-            ...params.InputProps,
-            type: 'search',
-            endAdornment: (
-              <InputAdornment position="start">
-                <IconButton>
-                  <Search />
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-        />
-    )}
-  ></Autocomplete>
-);
+const currentPosition = {
+  lat:0, lng:0
+}
 
 function Home() {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      currentPosition.lat = position.coords.latitude
+      currentPosition.lng = position.coords.longitude
+    });
+  })
+
+  // Set value for search
+  const [search, setSearch] = React.useState('')
+
+  const searchHandler = (value) => {
+    console.log(value)
+    setSearch(value)
+  }
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: 'AIzaSyDfOebxIElD6W5NhOHVSgnF85v61drGRDk'
   })
 
   const [map, setMap] = React.useState(null)
-
   const onLoad = React.useCallback(function callback(map) {
     // only example for getting and using the map instance
-    map.setZoom(zoom)
+    map.setZoom(6)
 
     setMap(map)
   }, [])
@@ -83,6 +67,11 @@ function Home() {
   const onUnmount = React.useCallback(function callback(map) {
     setMap(map)
   }, [map])
+
+  let navigate = useNavigate();
+  const markerClick = (path) => {
+    navigate(path);
+  }
 
   return isLoaded ? (
     <Box sx={{width:"100%", flexGrow: 1, height:"93vh"}}>
@@ -92,25 +81,59 @@ function Home() {
           <Typography>Click a marker below or search for City, route</Typography>
         </Grid>
         <Grid item sx={{mt:{xs:2, sm:2, md:0}}} xs={12} md={8}>
-          <SearchBar />
+          {/* KWAON ANG VALUE SA SEARCHBAR */}
+          <Autocomplete
+            freeSolo={true}
+            id="free-solo-2-demo"
+            disableClearable
+            value={search} 
+            onChange={(event, value)=> searchHandler(value)}
+            options={searchLists}
+            renderInput={(params) => (        
+                <TextField
+                  {...params}
+                  label="Search a city, location, route name,"
+                  InputProps={{
+                    ...params.InputProps,
+                    type: 'search',
+                    endAdornment: (
+                      <InputAdornment position="start">
+                        <IconButton>
+                          <Search />
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                />
+            )}
+          ></Autocomplete>
         </Grid>
       </Grid>
       <GoogleMap
         sx={{height:"92vh"}}
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={zoom}
+        zoom={6}
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
         {
           markers.map((mark) => (
-            <Marker
+            <MarkerF
               key={mark.name}
+              title={mark.name}
+              onClick={() => markerClick(mark.path)}
               position={mark.position}
+              zIndex="999"
             />
           ))
         }
+        <MarkerF
+          title="Current Location"
+          zIndex="1"
+          label="You're here"
+          position={currentPosition}
+        />
       </GoogleMap>
     </Box>
   ) : <></>
