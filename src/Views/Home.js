9929ad58781919d335a093bb471a1800250f347d
Kevin Aton <kevinaton@Kevins-MacBook-Pro.local>
2023-02-10 00:40:05 +0800

    Initial implementation of search result, showing the filtered results replacing the map

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api';
-import { Box } from '@mui/system';
-import { Autocomplete, Grid, IconButton, InputAdornment, TextField, Typography } from '@mui/material';
+import { Autocomplete, Box, Card, CardActions, CardContent, Divider, Grid, IconButton, InputAdornment, Link, TextField, Typography } from '@mui/material';
 import data from "../assets/masterList.json"
 import { Search } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
@@ -50,19 +49,36 @@ function Home() {
     });
   })
 
-  // Set value for search
-  const [search, setSearch] = React.useState(null)
+  // Search functionality, it will provide the list of routes that is searched.
+  const [searchValue, setSearchValue] = React.useState('')
   const [result, setResults] = React.useState([])
   const [isDisplayResult, setDisplayResult] = React.useState(false)
 
-  const searchHandler = (value) => {
-    let loweredCase = value.toLowerCase()
-    setResults(
-      MasterList.filter(searched => {
-        setSearch(value)
-        return searched.name.toLowerCase().includes(loweredCase)
-      })
+  const searchHandler = (event, value) => {
+    setSearchValue(value)
+
+    // Search for city
+    const searchCity = MasterList[0].submenu.filter(
+      (item) => item.name.toLowerCase().includes(searchValue.toLowerCase())
     )
+    // Search for route
+    const searchRoute = MasterList.slice(1).filter(
+      (item) => item.submenu.filter(
+        (subitem)=>subitem.name.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    )
+
+    const searchLocations = MasterList.slice(1).filter(
+      (item) => item.submenu.filter(
+        (subitem) => subitem.locations.filter(
+          (location)=>location.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      )
+    )
+
+    const setResult = [...searchCity, ...searchRoute, ...searchLocations]
+
+    setResults([...new Set(setResult)])
   }
 
   const checkSearchInput = (value) => {
@@ -114,13 +130,12 @@ function Home() {
           <Typography>Click a marker below or search for City, route</Typography>
         </Grid>
         <Grid item sx={{mt:{xs:2, sm:2, md:0}}} xs={12} md={8}>
-
         <Autocomplete
           freeSolo={true}
           id="free-solo-2-demo"
           disableClearable
-          value={search} 
-          onChange={(event, value)=> searchHandler(value)}
+          value={searchValue} 
+          onChange={searchHandler}
           onInputChange={(event, value, reason)=>checkSearchInput(value,reason)}
           options={searchLists}
           renderInput={(params) => (        
@@ -143,7 +158,7 @@ function Home() {
         ></Autocomplete>
         </Grid>
       </Grid>
-      <Box style={{ width:"100%", height:"100%", display: isDisplayResult ? "none" : "block" }}>
+      <Box sx={{ width:"100%", height:"100%", display: isDisplayResult ? "none" : "block" }}>
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
@@ -170,6 +185,47 @@ function Home() {
           }
         </GoogleMap>
       </Box>
+      <Box sx={{ p:3, display: isDisplayResult ? "block" : "none"}}>
+      <Divider></Divider>
+      <Typography variant="h6" sx={{mt:2}}>Here are the results:</Typography>
+      <Grid container spacing={2} sx={{mt:1}}>
+      {
+        // data.data.id must be equal to city's id route
+        result.map((data, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index} alignItems="left" justify="center">
+          <Card variant="outlined">
+            <CardContent sx={{ height:100 }}>
+              <Link
+                underline='none'
+                variant='h5'
+                color="primary"
+                href={'/' + data.path}
+                sx={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "-webkit-box",
+                  WebkitLineClamp: "1",
+                  WebkitBoxOrient: "vertical",
+                }}
+              >{data.name}</Link>
+              <Typography
+                sx={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  display: "-webkit-box",
+                  WebkitLineClamp: "2",
+                  WebkitBoxOrient: "vertical",
+                }}
+              >{data.name}</Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+            </CardActions>
+          </Card>
+          </Grid>
+        ))
+      }
+      </Grid>
+    </Box>
     </Box>
   ) : <></>
 }
